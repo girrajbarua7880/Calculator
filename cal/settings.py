379140e850527852e16c6a677f9acf1cf4876309
Girraj Barua <girrajbarua7880@gmail.com>
2026-02-13 02:09:35 +0530

    correction on setting comma

--- cal/settings.py
+++ cal/settings.py
@@ -43,10 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
